7a4d087537f4a9629950202cec318cddb575fc48
Randi <randi.junior@55pbx.com.br>
2022-07-12 07:59:34 -0300

    progress

--- src/boot/db.ts
+++ src/boot/db.ts
@@ -1,6 +1,20 @@
-import mongoose from "mongoose";
+import mongoose, {Connection} from "mongoose";
 
-export default async () => {
-    let { connection } = await mongoose.connect("mongodb://localhost:27017/", {});
-    return connection;
+export default async (
+    host: string,
+    port: string | number,
+    name: string,
+    user: string,
+    pass: string,
+): Promise<Connection> =>  {
+
+    return new Promise((resolve) => {
+        let conn =  mongoose.createConnection(`mongodb://${user}:${pass}@${host}:${port}/${name}`);
+
+        conn.on("open", () => {
+            console.log(`Connected to MongoDB "${host}:${port}/${name}"`);
+            resolve(conn);
+        });
+    });
+
 }
